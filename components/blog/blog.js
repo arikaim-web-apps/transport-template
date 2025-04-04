8f3b87509f4277450ac220047661dff1cb2aede3
arikaim <info@arikaim.com>
2025-04-04 22:33:12 +0300

    =

--- components/blog/blog.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-arikaim.component.onLoaded(function() {
-    arikaim.ui.preloadImages(function(image) {
-        $(image).addClass('opacity-100');
-        $(image).parent().find('.image-loader').remove();
-    });
-});
